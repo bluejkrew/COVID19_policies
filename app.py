c0ea9ef69c1cef94ab762db0ad757f45edbdd397
Jonathan Tsao <jonathantsao@Jonathans-MacBook-Pro.local>
2021-02-15 14:38:16 -0500

    inserted 2 placeholder paragraphs

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ def app():
 	st.title("Targeted Strategies in Reducing the Spread of COVID-19 and Future Pandemics")
 	# And write the rest of the app inside this function!
 	st.sidebar.header("Header 1")
-	st.markdown('''This is where I place my paragraph.''')
+	st.markdown('''This website is intended to assist public health officials and decision makers in identifying effective policies to combat a respiratory-spread pandemic based on county-specific features.
+
+The model is based on county-level disease data collected during the COVID-19 pandemic, combined with census data for county-level demographics. 
+.''')
 
 	st.code("x = 8 + 13")
 
