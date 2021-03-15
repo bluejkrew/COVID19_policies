f74c58327db3b24a3b8dffcfb5c7aa215ff92d4f
bluejkrew <66223206+bluejkrew@users.noreply.github.com>
2021-03-15 12:49:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,23 +9,23 @@ def app():
 	st.sidebar.header("Header 1")
 	st.markdown('''This website is intended to assist public health officials and decision makers in identifying effective policies to combat a respiratory-spread pandemic based on county-specific features.
 
-The model is based on county-level disease data collected during the COVID-19 pandemic, combined with census data for county-level demographics. 
-.''')
+	The model is based on county-level disease data collected during the COVID-19 pandemic, combined with census data for county-level demographics. 
+	.''')
 
 	st.code("x = 8 + 13")
 
 	st.sidebar.markdown('''Ahh yeah...bullet points!''')
 	
 	# Pandas example
-  st.markdown("""## Pandas
-Streamlit is made to render lots of Python objects such as Pandas DataFrames. Let's create a simple DataFrame and render it.
-  """)
-  df = pd.DataFrame({'x':[1,2,3], 'y':[40, 20, 30], 'z':[100, 40, 33]})
-  st.write(df)
-  st.markdown("""Remember that Streamlit will run any Python code you give it, so you can of course manipulate your DataFrame. Also, you can use Streamlit widgets for simple control flow and interactivity! Let's put some of these in the sidebar. We'll use the following code:
-  
-  ```python
-  func = st.sidebar.selectbox("Aggregate:", ['Sum', 'Mean', 'Median'])
+  	st.markdown("""## Pandas
+	Streamlit is made to render lots of Python objects such as Pandas DataFrames. Let's create a simple DataFrame and render it.
+  	""")
+  	df = pd.DataFrame({'x':[1,2,3], 'y':[40, 20, 30], 'z':[100, 40, 33]})
+  	st.write(df)
+  	st.markdown("""Remember that Streamlit will run any Python code you give it, so you can of course manipulate your DataFrame. Also, you can use Streamlit widgets for simple control flow and interactivity! Let's put some of these in the sidebar. We'll use the following code:
+  	
+  	```python
+  	func = st.sidebar.selectbox("Aggregate:", ['Sum', 'Mean', 'Median'])
 	
 	
 	
